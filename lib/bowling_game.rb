08644c7a67859182cb0a7ab7e6e75c534f198170
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-22 10:22:40 +0800

    [Stella]Refactor method score in the bowling game first attempt

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -5,15 +5,12 @@ class BowlingGame
     end
 
     def score
-
-        #the first index
         first_in_frame = 0
-        #frame is the total number of big frames
         frame = 0
         score = 0
         
         while frame < 10
-            if @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10 
+            if spare?(first_in_frame)
                 score += 10 + @rolls[first_in_frame+2]
             else
                 score += @rolls[first_in_frame] + @rolls[first_in_frame+1]
@@ -23,6 +20,12 @@ class BowlingGame
         end
 
         score
+    end
+
+    private
+
+    def spare?(first_in_frame)
+        @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10 
     end
 
 end
