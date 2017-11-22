eb1cd67fdcc9c7e79b66152457c811586772959d
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-22 10:30:29 +0800

    [Stella]Refactor calculating standard frame

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -17,7 +17,7 @@ class BowlingGame
             if spare?
                 score += 10 + bonus_for_spare
             else
-                score += @rolls[first_in_frame] + @rolls[first_in_frame+1]
+                score += standard_frame_score
             end
             frame += 1 
             @first_in_frame += 2
@@ -29,11 +29,15 @@ class BowlingGame
     private
 
     def spare?
-        @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10 
+        standard_frame_score == 10 
     end
 
     def bonus_for_spare
         @rolls[first_in_frame+2]
+    end
+
+    def standard_frame_score
+        @rolls[first_in_frame] + @rolls[first_in_frame+1]
     end
 
 end
