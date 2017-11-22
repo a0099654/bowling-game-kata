e400fd4c97fd6093da5795e0fd75dc8615e31871
Stella Widyasari <stellaw@thoughtworks.com>
2017-11-22 10:13:01 +0800

    [Stella]Implement method score in the bowling game first attempt

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,8 +1,24 @@
 class BowlingGame
-    attr_reader :score
 
     def pins(rolls)
-        @score = rolls.inject(:+)
+        @rolls = rolls
+    end
+
+    def score
+
+        #the first index
+        first_in_frame = 0
+        #frame is the total number of big frames
+        frame = 0
+        score = 0
+        
+        while frame < 10
+            score += @rolls[first_in_frame] + @rolls[first_in_frame+1]
+            frame += 1 #to increment this while loop
+            first_in_frame += 2
+        
+        end
+        score
     end
 
 end
